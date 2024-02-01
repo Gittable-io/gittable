a920a53f937ebc1da587cb1b680d9e22177a29ff
Habib <h.hourany@gmail.com>
2024-02-01 09:54:46 +0100

    init RepositoryList

---
+++ src/renderer/src/components/RepositoryList/RepositoryList.tsx
@@ -0,0 +1,5 @@
+import "./RepositoryList.css";
+
+export function RepositoryList(): JSX.Element {
+  return <div className="repository-list">RepositoriesList</div>;
+}
--- src/renderer/src/components/WelcomeSidebar/WelcomeSidebar.tsx
+++ src/renderer/src/components/WelcomeSidebar/WelcomeSidebar.tsx
@@ -1,10 +1,12 @@
+import { RepositoryList } from "../RepositoryList";
+
 import "./WelcomeSidebar.css";
 
 export function WelcomeSidebar(): JSX.Element {
   return (
     <div className="welcome-sidebar">
       <div className="logo">Gittable</div>
-      <div className="repositories-list">Repositories List</div>
+      <RepositoryList />
     </div>
   );
 }
