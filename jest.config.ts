b8b2391f9167a25c01ab5f2328038df95cada46c
Habib <h.hourany@gmail.com>
2024-02-01 18:21:20 +0100

    add Jest

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+// jest.config.ts
+
+/*
+It took me a while, but I was able to add jest support with my electron-vite project
+1. I installed ts-node : npm i ts-node -D
+2. I copied jest.config.ts (just the preset), in the future I may need to copy or add other configs
+*/
+export default {
+  preset: "ts-jest",
+};
