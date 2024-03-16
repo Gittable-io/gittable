99781eb69700c2235ef35e1da28a78b06c1ebc55
Habib <h.hourany@gmail.com>
2024-03-16 14:55:43 +0100

    now TableDiffViewer fetches its own data

--- src/renderer/src/components/repository-workspace/editor-panel-group/TableDiffViewerPanel/TableDiffViewerPanel.tsx
+++ src/renderer/src/components/repository-workspace/editor-panel-group/TableDiffViewerPanel/TableDiffViewerPanel.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback } from "react";
 import { DiffDescription } from "../EditorPanelGroup";
 import "./TableDiffViewerPanel.css";
 import { Table, TableDiffViewer } from "gittable-editor";
@@ -12,51 +12,43 @@ export function TableDiffViewerPanel({
   repositoryId,
   diffDescription,
 }: TableDiffViewerPanelProps): JSX.Element {
-  const [from, setFrom] = useState<Table | null>(null);
-  const [to, setTo] = useState<Table | null>(null);
-
-  const fetchTableData = useCallback(async (): Promise<[Table, Table]> => {
-    const responseFrom = await window.api.get_table_data({
+  const fetchFromTable = useCallback(async (): Promise<Table> => {
+    const response = await window.api.get_table_data({
       repositoryId,
       tableId: diffDescription.table.id,
       ref: diffDescription.fromRef,
     });
 
-    if (responseFrom.status !== "success") {
+    if (response.status !== "success") {
       throw new Error(
         `Error loading table data. repositoryId = ${repositoryId}, tableId=${diffDescription.table.id}, ref=${diffDescription.fromRef}`,
       );
     }
+    return response.tableData;
+  }, [repositoryId, diffDescription]);
 
-    const responseTo = await window.api.get_table_data({
+  const fetchToTable = useCallback(async (): Promise<Table> => {
+    const response = await window.api.get_table_data({
       repositoryId,
       tableId: diffDescription.table.id,
       ref: diffDescription.toRef,
     });
 
-    if (responseTo.status !== "success") {
+    if (response.status !== "success") {
       throw new Error(
         `Error loading table data. repositoryId = ${repositoryId}, tableId=${diffDescription.table.id}, ref=${diffDescription.toRef}`,
       );
     }
 
-    return [responseFrom.tableData, responseTo.tableData];
+    return response.tableData;
   }, [repositoryId, diffDescription]);
-
-  useEffect(() => {
-    async function fetchData(): Promise<void> {
-      const tables = await fetchTableData();
-      setFrom(tables[0]);
-      setTo(tables[1]);
-    }
-    fetchData();
-  }, [fetchTableData]);
 
   return (
     <div className="table-diff-viewer-panel">
-      {from != null && to != null && (
-        <TableDiffViewer fromTable={from} toTable={to} />
-      )}{" "}
+      <TableDiffViewer
+        fetchFromTable={fetchFromTable}
+        fetchToTable={fetchToTable}
+      />
     </div>
   );
 }
