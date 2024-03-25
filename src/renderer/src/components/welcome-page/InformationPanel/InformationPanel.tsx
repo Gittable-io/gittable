6957ff411be657bd2ab714023b8c283c5591134b
Habib <h.hourany@gmail.com>
2024-03-25 11:26:25 +0100

    Write help message in information panel

--- src/renderer/src/components/welcome-page/InformationPanel/InformationPanel.tsx
+++ src/renderer/src/components/welcome-page/InformationPanel/InformationPanel.tsx
@@ -1,5 +1,26 @@
 import "./InformationPanel.css";
 
 export function InformationPanel(): JSX.Element {
-  return <div className="information-panel">InformationPanel</div>;
+  return (
+    <div className="information-panel">
+      <div className="required-action">
+        <h2>One step left : Configure your Author Identity</h2>
+        <p>
+          You need to setup your user name and email that will be used to
+          identify you as the author of your contributions and facilitate
+          collaboration with your team.
+        </p>
+        <p>
+          Unsure about which name and email to use? It&apos;s generally best to
+          use your professional name and the email address associated with your
+          company account.
+        </p>
+        <p>
+          If you&apos;re still uncertain, consult you Git administrator or IT
+          department for the recommended user.name and user.email Git
+          configuration.
+        </p>
+      </div>
+    </div>
+  );
 }
