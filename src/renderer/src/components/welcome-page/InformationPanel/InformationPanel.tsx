b86a680540bf30ef968e06d40c5b0606dce51fc7
Habib <h.hourany@gmail.com>
2024-03-25 11:38:05 +0100

    ui: improve styling of help message

--- src/renderer/src/components/welcome-page/InformationPanel/InformationPanel.tsx
+++ src/renderer/src/components/welcome-page/InformationPanel/InformationPanel.tsx
@@ -4,21 +4,24 @@ export function InformationPanel(): JSX.Element {
   return (
     <div className="information-panel">
       <div className="required-action">
-        <h2>One step left : Configure your Author Identity</h2>
+        <h3>One step left : Configure your Author Identity</h3>
         <p>
-          You need to setup your user name and email that will be used to
-          identify you as the author of your contributions and facilitate
-          collaboration with your team.
+          You need to setup your user name and email to identify you correctly
+          as the author of your contributions and facilitate collaboration with
+          your team.
         </p>
         <p>
-          Unsure about which name and email to use? It&apos;s generally best to
-          use your professional name and the email address associated with your
-          company account.
+          <em>Unsure about which name and email to use?</em>
+          <br />
+          It&apos;s generally best to use your professional name and email
+          address associated with your company account.
         </p>
         <p>
-          If you&apos;re still uncertain, consult you Git administrator or IT
-          department for the recommended user.name and user.email Git
-          configuration.
+          <em>Still uncertain?</em>
+          <br />
+          Consult you Git administrator or IT department for the recommended{" "}
+          <code>user.name</code> and &nbsp;
+          <code>user.email</code> Git configuration.
         </p>
       </div>
     </div>
