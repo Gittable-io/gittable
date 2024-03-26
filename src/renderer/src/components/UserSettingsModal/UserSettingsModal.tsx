8e62438201093a1d92f606889a37e7471a60bc1a
Habib <h.hourany@gmail.com>
2024-03-26 08:43:25 +0100

    User settings : add user name & email form

--- src/renderer/src/components/UserSettingsModal/UserSettingsModal.tsx
+++ src/renderer/src/components/UserSettingsModal/UserSettingsModal.tsx
@@ -1,6 +1,7 @@
-import { MaterialSymbol } from "gittable-editor";
+import { Button, InputAndValidation } from "gittable-editor";
 import { Modal } from "../ui-components/Modal";
 import "./UserSettingsModal.css";
+import { useState } from "react";
 
 export type UserSettingsModalProps = {
   onClose: () => void;
@@ -9,12 +10,58 @@ export type UserSettingsModalProps = {
 export function UserSettingsModal({
   onClose,
 }: UserSettingsModalProps): JSX.Element {
+  const [gitUserName, setGitUserName] = useState("");
+  const [gitUserEmail, setGitUserEmail] = useState("");
+
+  const saveUserSettings = (): void => {};
+
   return (
     <Modal>
       <div className="user-settings-modal">
-        <div className="title-section">
-          <h1>User Settings</h1>
-          <MaterialSymbol symbol="close" onClick={onClose} label="Close" />
+        <h1>User Settings</h1>
+        <div className="user-settings-forms">
+          <div className="user-settings-form-section">
+            <div className="user-settings-form">
+              <div className="label-and-input">
+                <label>User name</label>
+                <InputAndValidation
+                  value={gitUserName}
+                  onChange={setGitUserName}
+                />
+              </div>
+              <div className="label-and-input">
+                <label>User email</label>
+                <InputAndValidation
+                  value={gitUserEmail}
+                  onChange={setGitUserEmail}
+                />
+              </div>
+            </div>
+            <div className="user-settings-form-information">
+              <p>
+                The user <strong>name</strong> and <strong>email</strong> will
+                be attached to your contributions to correctly identify you as
+                the author.
+              </p>
+              <p>
+                <em>Unsure about which name and email to use?</em>
+                <br />
+                It&apos;s generally best to use your professional name and email
+                address associated with your company account.
+              </p>
+              <p>
+                <em>Still uncertain?</em>
+                <br />
+                Consult you Git administrator or IT department for the
+                recommended <code>user.name</code> and &nbsp;
+                <code>user.email</code> Git configuration.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="button-group">
+          <Button text="Cancel" variant="outlined" onClick={onClose} />
+          <Button text="Save" variant="contained" onClick={saveUserSettings} />
         </div>
       </div>
     </Modal>
