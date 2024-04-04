b2d6ec60cccb73113361bd3b0d60039f5228c37f
Habib <h.hourany@gmail.com>
2024-04-04 09:30:01 +0200

    Show spinner when pull is in progress

--- src/renderer/src/components/repository-workspace/source-control/PullButton/PullButton.tsx
+++ src/renderer/src/components/repository-workspace/source-control/PullButton/PullButton.tsx
@@ -1,17 +1,23 @@
 import { MaterialSymbolButton } from "gittable-editor";
+import { useState } from "react";
 
 export type PullButtonProps = {
   repositoryId: string;
 };
 
 export function PullButton({ repositoryId }: PullButtonProps): JSX.Element {
+  const [pullInProgress, setPullInProgress] = useState<boolean>(false);
+
   const pull = async (): Promise<void> => {
+    setPullInProgress(true);
     const response = await window.api.pull({ repositoryId });
     if (response.status === "error") {
       console.error(`[PullButton] Error pulling`);
     } else {
       console.debug(`[PullButton] Fetch success`);
     }
+
+    setPullInProgress(false);
   };
 
   return (
@@ -20,6 +26,7 @@ export function PullButton({ repositoryId }: PullButtonProps): JSX.Element {
       label="Pull changes"
       onClick={pull}
       tooltip
+      loading={pullInProgress}
     />
   );
 }
