998ebebeada424cfa7752d7d0cd44c9a1da54ea1
Habib <h.hourany@gmail.com>
2024-04-05 13:07:00 +0200

    feat: close repository

--- src/renderer/src/components/repository-workspace/RepositoryWorkspaceSidebar2/RepositoryWorkspaceSidebar2.tsx
+++ src/renderer/src/components/repository-workspace/RepositoryWorkspaceSidebar2/RepositoryWorkspaceSidebar2.tsx
@@ -1,9 +1,20 @@
+import { MaterialSymbolButton } from "gittable-editor";
 import "./RepositoryWorkspaceSidebar2.css";
+import { appActions } from "@renderer/store/appSlice";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "@renderer/store/store";
 
 export function RepositoryWorkspaceSidebar2(): JSX.Element {
+  const dispatch = useDispatch<AppDispatch>();
+
   return (
     <div className="repository-workspace-sidebar2">
-      RepositoryWorkspaceSidebar2
+      <div className="toolbar">
+        <MaterialSymbolButton
+          symbol="close"
+          onClick={() => dispatch(appActions.closeRepository())}
+        />
+      </div>
     </div>
   );
 }
