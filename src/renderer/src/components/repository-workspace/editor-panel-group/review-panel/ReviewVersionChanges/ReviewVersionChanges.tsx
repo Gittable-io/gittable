989d15c365d411f494f823e1a8c97cafe3b8978c
Habib <h.hourany@gmail.com>
2024-04-16 10:45:44 +0200

    display base published version

--- src/renderer/src/components/repository-workspace/editor-panel-group/review-panel/ReviewVersionChanges/ReviewVersionChanges.tsx
+++ src/renderer/src/components/repository-workspace/editor-panel-group/review-panel/ReviewVersionChanges/ReviewVersionChanges.tsx
@@ -1,4 +1,4 @@
-import { VersionContentComparison } from "@sharedTypes/index";
+import { DraftVersion, VersionContentComparison } from "@sharedTypes/index";
 import { useEffect, useState } from "react";
 import { List, ListItem } from "gittable-editor";
 import { useSelector } from "react-redux";
@@ -10,8 +10,8 @@ export function ReviewVersionChanges(): JSX.Element {
   const repositoryId = useSelector(
     (state: AppRootState) => state.repo.repository!.id,
   );
-  const currentVersion = useSelector(
-    (state: AppRootState) => state.repo.currentVersion!,
+  const currentVersion: DraftVersion = useSelector(
+    (state: AppRootState) => state.repo.currentVersion! as DraftVersion,
   );
 
   const commits = useSelector(
@@ -41,7 +41,12 @@ export function ReviewVersionChanges(): JSX.Element {
 
   return (
     <div className="review-version-changes">
-      <h2>Changes from last published version</h2>
+      <h2>
+        Changes since version{" "}
+        {currentVersion.basePublishedVersion === "INITIAL"
+          ? "start"
+          : currentVersion.basePublishedVersion.name}
+      </h2>
       <List>
         {diff.map((tableDiff) => (
           <ListItem
